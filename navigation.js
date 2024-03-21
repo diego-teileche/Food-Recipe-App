6039b9e38942a712a37db656ade320e8b6c167ca
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-03-21 19:06:41 -0300

    building welcome screen

---
+++ navigation.js
@@ -0,0 +1,22 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import HomeScreen from "./screens/HomeScreen"
+import WelcomeScreen from "./screens/WelcomeScreen"
+
+const Stack = createNativeStackNavigator()
+
+function Navigation() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				initialRouteName="Welcome"
+				screenOptions={{ headerShown: false }}
+			>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Welcome" component={WelcomeScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
+}
+
+export default Navigation
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,61 @@
+import { View, Text, Image } from "react-native"
+import React, { useEffect } from "react"
+import { StatusBar } from "expo-status-bar"
+import {
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp,
+} from "react-native-responsive-screen"
+import Animated, { useSharedValue, withSpring } from "react-native-reanimated"
+
+export default function WelcomeScreen() {
+	const ring1padding = useSharedValue(0)
+	const ring2padding = useSharedValue(0)
+
+	useEffect(() => {
+		ring1padding.value = 0
+		ring2padding.value = 0
+
+		setTimeout(() => {
+			ring1padding.value = withSpring(ring1padding.value + hp(5))
+		}, 100)
+		setTimeout(() => {
+			ring2padding.value = withSpring(ring2padding.value + hp(5.5))
+		}, 300)
+	}, [])
+
+	return (
+		<View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
+			<StatusBar style="light" />
+
+			<Animated.View
+				className="bg-white/20 rounded-full"
+				style={{ padding: ring2padding }}
+			>
+				<Animated.View
+					className="bg-white/20 rounded-full"
+					style={{ padding: ring1padding }}
+				>
+					<Image
+						source={require("../assets/images/mongo.webp")}
+						style={{ width: hp(20), height: hp(20) }}
+					/>
+				</Animated.View>
+			</Animated.View>
+
+			<View className="flex items-center space-y-2">
+				<Text
+					className="font-bold text-white tracking-widest"
+					style={{ fontSize: hp(7) }}
+				>
+					Foody
+				</Text>
+				<Text
+					className="font-medium text-white tracking-widest"
+					style={{ fontSize: hp(2) }}
+				>
+					Food is always right
+				</Text>
+			</View>
+		</View>
+	)
+}
