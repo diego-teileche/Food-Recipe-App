c6f52c71103226693d489f4e143972c6ba120505
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-03-22 17:23:14 -0300

    building home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,68 @@
-import { View, Text } from "react-native"
 import React from "react"
-import { SafeAreaView } from "react-native-safe-area-context"
+import { View, Text, Image, ScrollView, TextInput } from "react-native"
+import { StatusBar } from "expo-status-bar"
+import {
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp,
+} from "react-native-responsive-screen"
+import { BellIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline"
+import Categories from "../components/categories"
 
 export default function HomeScreen() {
 	return (
-		<SafeAreaView>
-			<Text>HomeScreen</Text>
-		</SafeAreaView>
+		<View className="flex-1 bg-white">
+			<StatusBar style="dark" />
+
+			<ScrollView
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{ paddingBottom: 50 }}
+				className="space-y-6 pt-14"
+			>
+				<View className="mx-4 flex-row justify-between items-center mb-2">
+					<Image
+						source={require("../assets/images/profileMessi.jpeg")}
+						style={{ height: hp(5), width: hp(5.5) }}
+						className="rounded-full"
+					/>
+					<BellIcon size={hp(4)} color="gray" />
+				</View>
+
+				<View className="mx-4 space-y-2 mb-2">
+					<Text style={{ fontSize: hp(1.7) }} className="text-neutral-600">
+						Hello, Messi!
+					</Text>
+					<View>
+						<Text
+							style={{ fontSize: hp(3.8) }}
+							className="font-semibold text-neutral-600"
+						>
+							Make your own food
+						</Text>
+					</View>
+					<Text
+						style={{ fontSize: hp(3.8) }}
+						className="font-semibold text-neutral-600"
+					>
+						Stay at <Text className="text-amber-400">home</Text>
+					</Text>
+				</View>
+
+				<View className="mx-4 flex-row items-center rounded-full bg-black/20 p-[6px]">
+					<TextInput
+						placeholder="Search any recipe"
+						placeholderTextColor={"black"}
+						style={{ fontSize: hp(1.7) }}
+						className="flex-1 text-base pl-3 tracking-wider"
+					/>
+					<View className="bg-white rounded-full p-3">
+						<MagnifyingGlassIcon size={hp(2.5)} strokeWidth={3} color="gray" />
+					</View>
+				</View>
+
+				<View>
+					<Categories />
+				</View>
+			</ScrollView>
+		</View>
 	)
 }
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -6,10 +6,13 @@ import {
 	heightPercentageToDP as hp,
 } from "react-native-responsive-screen"
 import Animated, { useSharedValue, withSpring } from "react-native-reanimated"
+import { useNavigation } from "@react-navigation/native"
 
 export default function WelcomeScreen() {
 	const ring1padding = useSharedValue(0)
 	const ring2padding = useSharedValue(0)
+
+	const navigation = useNavigation()
 
 	useEffect(() => {
 		ring1padding.value = 0
@@ -18,9 +21,14 @@ export default function WelcomeScreen() {
 		setTimeout(() => {
 			ring1padding.value = withSpring(ring1padding.value + hp(5))
 		}, 100)
+
 		setTimeout(() => {
 			ring2padding.value = withSpring(ring2padding.value + hp(5.5))
 		}, 300)
+
+		setTimeout(() => {
+			navigation.navigate("Home")
+		}, 2500)
 	}, [])
 
 	return (
